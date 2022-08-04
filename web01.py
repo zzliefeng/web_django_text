1939818dd96f4661e38a8e524e34057a7e900f87
ming <zzliefeng@aliyun.com>
2022-08-05 02:08:13 +0800

    123

--- web01.py
+++ web01.py
@@ -1,1 +1,23 @@
-3132312312765756-0-0-09999
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route('/register', methods=['GET'])
+def register():
+    return render_template('/register.html')
+
+#
+# @app.route("/do/reg", methods=['GET'])
+# def get_register():
+#     return "注册成功"
+
+
+@app.route("/post/reg", methods=["POST"])
+def post_register():
+    print(request.form)
+    return "注册成功"
+
+
+if __name__ == '__main__':
+    app.run()
